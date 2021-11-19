a2da3feded70418569177fa6d4ce40e89026b59f
thomasravache <thomasravache31@gmail.com>
2021-11-19 15:36:19 -0300

    Realocando estilos

---
+++ src/components/tailwindStyles/aboutStyles.js
@@ -0,0 +1,11 @@
+const styles = {
+  aboutContainer: 'container px-4 mx-auto',
+  aboutContentContainer: 'lg:space-x-5 lg:flex lg:flex-row items-center lg:mx-4 flex flex-col-reverse text-center lg:text-left',
+  aboutPersonalInfo: 'lg:px-4 lg:mt-12',
+  aboutHeading: 'text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white',
+  aboutTextContainer: 'mt-6 text-gray-800 dark:text-white',
+  aboutImageContainer: 'flex-shrink-0 lg:mt-12 lg:px-4 mb-10',
+  aboutImage: 'rounded-full',
+}
+
+export default styles;
