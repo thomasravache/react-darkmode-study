3fca108401f3e8bcce68876b4c43a4b6365b39bf
thomasravache <thomasravache31@gmail.com>
2021-11-19 15:21:48 -0300

    Adicionando estilos tailwind

---
+++ src/tailwindStyles/navStyles.js
@@ -0,0 +1,7 @@
+const styles = {
+  navContainer: 'sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black',
+  navSubContainer: 'container px-4 mx-auto lg:max-w-4xl flex items-center justify-between',
+  navLink: 'font-medium tracking-wider transition-colors text-gray-900 hover:text-pink-500 uppercase dark:text-white dark:hover:text-pink-400',
+};
+
+export default styles;
