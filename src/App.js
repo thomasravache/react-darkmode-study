27b3ba7b5ecb50abfd789bc85a4fed3843a4be24
thomasravache <thomasravache31@gmail.com>
2021-11-19 17:08:28 -0300

    Adicionando alteracoes ao componente app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navigation />
+      <About />
+      <Footer />
     </div>
   );
 }
